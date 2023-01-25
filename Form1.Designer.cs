d26ae779bad7b7f757f93f62683da5d7383481b2
sallenmosele <sallen_adri@hotmail.com>
2023-01-24 21:22:14 -0300

    calculadora imc, desenvolvido com a linguagem c# e windows forms

---
+++ Form1.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace imcpesoideal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(519, 364);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.ComboBox cmbSexo;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnPesoIdeal;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAltura;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.ComponentModel;
+
+using System.Data;
+
+using System.Drawing;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+
+
+namespace imcpesoideal
+
+{
+
+    public partial class Form1 : Form
+
+    {
+
+        public Form1()
+
+        {
+
+            InitializeComponent();
+
+        }
+
+
+
+        private void btnSair_Click(object sender, EventArgs e)
+
+        {
+
+            this.Close();
+
+        }
+
+
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+
+        {
+
+            txtName.Clear();
+
+            txtAltura.Clear();
+
+            cmbSexo.Text = "";
+
+            txtName.Focus();
+
+        }
+
+
+
+        private void btnPesoIdeal_Click(object sender, EventArgs e)
+
+        {
+
+            Double Altura = 0, Pesoideal = 0;
+
+            Altura = double.Parse(txtAltura.Text);
+
+            if (cmbSexo.Text == "Feminino")
+
+            {
+
+                Pesoideal = (62.7 * Altura) - 44.7;
+
+                MessageBox.Show(txtName.Text + "Seu peso ideal é:  " + Pesoideal.ToString(), "Peso Ideal", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+
+
+            else if (cmbSexo.Text == "Masculino")
+
+            {
+
+                Pesoideal = (72.7 * Altura) - 58;
+
+                MessageBox.Show(txtName.Text + "Seu peso ideal é:  " + Pesoideal.ToString(), "Peso Ideal", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+
+
+            else
+
+            {
+
+                MessageBox.Show("Escolha o sexo", "Peso ideal", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+
+        }
+
+    }
+
+}
